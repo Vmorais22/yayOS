24b3584ff17ae08546f3b618fcfa271a7893aa7e
Vmorais22 <victormoraisllahi@gmail.com>
2021-03-06 18:08:29 +0100

    desafio condicional, logo cambiado, todo contenido de muestra

--- src/assets/MainMenuData.js
+++ src/assets/MainMenuData.js
@@ -1,5 +1,12 @@
-import photo1 from './images/posible1.png';
-import photo2 from './images/photo3.png';
+import photo1 from './images/photo1.png';
+import photo2 from './images/photo2.jpg';
+import photo3 from './images/photo3.png';
+import photo4 from './images/photo4.jpg';
+import photo5 from './images/photo5.jpg';
+import photo6 from './images/photo6.jpg';
+import photo7 from './images/photo7.jpg';
+import photo8 from './images/photo8.png';
+import photo9 from './images/photo9.jpg';
 
 export const lessonsInfo  = [
     {
@@ -21,48 +28,48 @@ export const lessonsInfo  = [
         banner: 'DIFICULTAD: FÁCIL',
         title: 'Ten tu propio correo electrónico',
         description: 'Seguro que has oído hablar de él. Paso a paso, crearás tu propio correo electrónico y descubrirás como mandar y recibir mensajes de forma instantánea. ¡Y no solo texto!',
-        image: photo1,
+        image: photo3,
     },
     {
         id: 4,
         banner: 'DIFICULTAD: MEDIA',
         title: 'Hagámos una videollamada ahora',
         description: '¿Deseas poder hablar cara a cara con tus seres queridos? ¿Por muy lejos que estén? No te preocupes, aquí todo es posible.',
-        image: photo1,
+        image: photo4,
     },
     {
         id: 5,
         banner: 'DIFICULTAD: FÁCIL',
         title: 'Ubicándonos con Google Maps',
         description: 'Una oportunidad de poder viajar a cualquier parte del mundo desde el salón de tu casa. ¡Aprende a usar los mapas de Google!',
-        image: photo2,
+        image: photo5,
     },
     {
         id: 6,
         banner: 'DIFICULTAD: MEDIA',
         title: 'Encuentra en la red lo que buscas',
         description: 'Se calcula que se hacen más de 6.000.0000.000 de búsquedas por Internet al día. ¿Añadimos unas cuántas más al recuento?',
-        image: photo1,
+        image: photo6,
     },
     {
         id: 7,
         banner: 'DIFICULTAD: FÁCIL',
         title: 'Gestionando los ficheros de mi ordenador',
         description: 'Un ordenador es, a fin de cuentas, una máquina capaz de almacenar información. ¿Qué te parece si miramos cómo guardar cosas en él?',
-        image: photo1,
+        image: photo7,
     },
     {
         id: 8,
         banner: 'DIFICULTAD: DIFICIL',
         title: 'Comprando por Internet de forma segura',
         description: 'A veces las mejores ofertas o simplemente aquello que buscamos no está cerca de donde vivimos. ¿Y si tratamos de comprarlo online?',
-        image: photo1,
+        image: photo8,
     },
     {
         id: 9,
         banner: 'DIFICULTAD: DIFICIL',
         title: 'Teniendo mi propia red social',
         description: 'Una red social es la mejor herramienta para mantener el contacto con los que lo hemos perdido. ¿Recuperamos amistades del pasado?',
-        image: photo1,
+        image: photo9,
     },
 ];
