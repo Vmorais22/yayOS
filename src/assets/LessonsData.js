d459ee7491285c1b968db85274d0961e838874bb
Vmorais22 <victormoraisllahi@gmail.com>
2021-03-17 18:59:58 +0100

    1.5

--- src/assets/LessonsData.js
+++ src/assets/LessonsData.js
@@ -1,19 +1,34 @@
 import img11 from "./images/1.1.png"
-import img12 from "./images/1.2.png"
+import img12 from "./images/1.4.png"
+import img13 from "./images/1.5.png"
+import img14 from "./images/1.2.png"
+import img15 from "./images/1.3.png"
 
 export const lessonsData = [
     {
         id: 1,
         title: 'Teclado y ratón',
-        numeroBotones: 2,
+        numeroBotones: 5,
         contenido: [
             {
                 textoBoton: 'Introducción al ratón',
                 img: img11,
             },
             {
-                textoBoton: 'Los botones del ratón',
+                textoBoton: 'Los botones del ratón: apretar',
+                img: img14,
+            },
+            {
+                textoBoton: 'Los botones del ratón: mantener',
+                img: img15,
+            },
+            {
+                textoBoton: 'Tipos de cursos - Parte 1',
                 img: img12,
+            },
+            {
+                textoBoton: 'Tipos de cursor - Part 2',
+                img: img13,
             },
         ],
 
