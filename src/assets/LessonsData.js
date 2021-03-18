194a9a9e5832d3e88ae5b62c9a5af6cb33dcd15e
Vmorais22 <victormoraisllahi@gmail.com>
2021-03-18 11:14:32 +0100

    all photos lesson 1

--- src/assets/LessonsData.js
+++ src/assets/LessonsData.js
@@ -1,18 +1,32 @@
 import img11 from "./images/1.1.png"
-import img12 from "./images/1.4.png"
-import img13 from "./images/1.5.png"
-import img14 from "./images/1.2.png"
-import img15 from "./images/1.3.png"
+import img12 from "./images/1.2.png"
+import img13 from "./images/1.3.png"
+import img14 from "./images/1.4.png"
+import img15 from "./images/1.5.png"
+import img16 from "./images/1.6.png"
+import img17 from "./images/1.7.png"
+import img18 from "./images/1.8.png"
+import img19 from "./images/1.9.png"
+import img110 from "./images/1.10.png"
+import img111 from "./images/1.11.png"
 
 export const lessonsData = [
     {
         id: 1,
         title: 'Teclado y ratón',
-        numeroBotones: 5,
+        numeroBotones: 12,
         contenido: [
             {
                 textoBoton: 'Introducción al ratón',
                 img: img11,
+            },
+            {
+                textoBoton: 'Tipos de cursor - Parte 1',
+                img: img12,
+            },
+            {
+                textoBoton: 'Tipos de cursor - Parte 2',
+                img: img13,
             },
             {
                 textoBoton: 'Los botones del ratón: apretar',
@@ -23,12 +37,32 @@ export const lessonsData = [
                 img: img15,
             },
             {
-                textoBoton: 'Tipos de cursos - Parte 1',
-                img: img12,
+                textoBoton: 'Posición correcta de la mano',
+                img: img16,
             },
             {
-                textoBoton: 'Tipos de cursor - Part 2',
-                img: img13,
+                textoBoton: 'Introducción al teclado',
+                img: img17,
+            },
+            {
+                textoBoton: 'Teclas sencillitas',
+                img: img18,
+            },
+            {
+                textoBoton: 'Poniendo tildes',
+                img: img19,
+            },
+            {
+                textoBoton: 'Combinando teclas',
+                img: img110,
+            },
+            {
+                textoBoton: 'Resumen de las teclas',
+                img: img111,
+            },
+            {
+                textoBoton: '¡Prueba tú ahora!',
+                img: img111,
             },
         ],
 
