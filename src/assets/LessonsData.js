61ca2aa0c99221330ee47e129a85b311817fe425
Vmorais22 <victormoraisllahi@gmail.com>
2021-03-19 16:29:05 +0100

    segunda leccion completa

--- src/assets/LessonsData.js
+++ src/assets/LessonsData.js
@@ -9,6 +9,15 @@ import img18 from "./images/1.8.png"
 import img19 from "./images/1.9.png"
 import img110 from "./images/1.10.png"
 import img111 from "./images/1.11.png"
+import img21 from "./images/2.1.png"
+import img22 from "./images/2.2.png"
+import img23 from "./images/2.3.png"
+import img24 from "./images/2.4.png"
+import img25 from "./images/2.5.png"
+import img26 from "./images/2.6.png"
+import img27 from "./images/2.7.png"
+import img28 from "./images/2.8.png"
+import img29 from "./images/2.9.png"
 
 export const lessonsData = [
     {
@@ -70,8 +79,45 @@ export const lessonsData = [
     {
         id: 2,
         title: 'Teclado y ratón',
-        numeroBotones: 12,
-        contenido: [ ],
+        numeroBotones: 9,
+        contenido: [
+            {
+                textoBoton: 'Qué es una cuenta de Google',
+                img: img21,
+            },
+            {
+                textoBoton: 'Qué son las pestañas',
+                img: img22,
+            },
+            {
+                textoBoton: 'Cómo cambiar de pestaña',
+                img: img23,
+            },
+            {
+                textoBoton: 'Buscar al registro de Google',
+                img: img24,
+            },
+            {
+                textoBoton: 'Acceder al registro de Google',
+                img: img25,
+            },
+            {
+                textoBoton: 'Registro en Google - Parte 1',
+                img: img26,
+            },
+            {
+                textoBoton: 'Registro en Google - Parte 2',
+                img: img27,
+            },
+            {
+                textoBoton: 'Registro en Google - Parte 3',
+                img: img28,
+            },
+            {
+                textoBoton: '¡Todo listo!',
+                img: img29,
+            },
+        ],
     },
 
 ];
