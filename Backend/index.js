2784b77315edd85a919404305cf2e1be74d61960
Vmorais22 <victormoraisllahi@gmail.com>
2021-04-16 18:03:18 +0200

    testing

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,19 @@
 'use strict'
 
+require('dotenv').config({path: '../src/var.env'});
 
 const mongoose = require('mongoose');
 const app = require('./app')
-const port = 3900;
-
+//const port = 3900;
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3900;
 /*CONEXIÖN MONGODB A NODE*/
+
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/yayos', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
         .then(() => {
             console.log('La conexión a la base de datos correcta');
-
             /*SERVER*/
             app.listen(port, () => {
                 console.log("servidor creado en puerto " + port)
